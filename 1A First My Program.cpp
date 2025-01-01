e95edecf6c208a14fd480c53f96c609d55c9c808
User - D?git config --global user.email bdulakun@gmail.com <bdulakun@gmail.com>
2025-01-02 02:16:55 +0700

    first commit

---
+++ 1A First My Program.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+	int main(){
+		int a = 90;
+		int b = 88;
+		int c = a + b;
+		
+		cout<<"HASIL DARI "<<a<<"+"<<b<<"="<<c<<endl<<endl;
+		cout<<"ANJAYYY BENEER :)"<<endl;
+		cin.get();
+		
+	}
+	
+
